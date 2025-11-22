b89c7921881ea4bbaa42646a84028283736f038b
vigneshwaran <vigneshvenketasan@gmail.com>
2025-11-22 20:50:12 +0530

    UseReducer method

--- src/UseReducer.jsx
+++ src/UseReducer.jsx
@@ -1,34 +1,69 @@
-import React, { useReducer } from 'react'
+import React, { useReducer } from "react";
 
+function counterReducer(state, action) {
+  switch (action.type) {
+    case "INCREMENT":
+      return { count: state.count + 1 };
 
-function counterReducer(state , action ){
+    case "DECREMENT":
+      return { count: state.count - 1 };
 
-    switch(action.type){
-        case "INCREMENT" : return {count : state.count +1}
+    case "RESET":
+      return { count: 0 };
 
-        case "DECREMENT" : return { count : state.count-1}
-
-        case "RESET" : return { count : 0}
-        default : return state
-    } 
-
+    case "SETVALUE":
+      return { count: action.payload };
+    default:
+      return state;
+  }
 }
 
 function UseReducer() {
+  const [state, dispatch] = useReducer(counterReducer, { count: 0 });
 
-    const [state , dispatch] = useReducer(counterReducer , {count: 0 })
-   
   return (
-    <div className='align-items m-4'>
-        <h1 className='text-center'>Use Reducer Method</h1>
-        <p className='text-center h3 '>count {state.count}</p>
+    <>
+      <div className="align-items m-4">
+        <h1 className="text-center">Use Reducer Method</h1>
+        <p className="text-center h3 ">count {state.count}</p>
 
-        <button className={"btn btn-primary m-3 "  }onClick={()=> {dispatch({type : "INCREMENT"})}}>Increament</button>
-        <button  className={"btn btn-danger" } disabled={ state.count === 0} onClick={() => dispatch({type :"DECREMENT"})}>Decrement</button>
-        <button className={"btn btn-secondary m-3" } onClick ={() => dispatch( {type:"RESET"})}>Reset</button>
+        <div className="text-center">
+            <button
+          className={"btn btn-primary m-3 "}
+          onClick={() => {
+            dispatch({ type: "INCREMENT" });
+          }}
+        >
+          Increament
+        </button>
+        <button
+          className={"btn btn-danger "}
+          disabled={state.count === 0}
+          onClick={() => dispatch({ type: "DECREMENT" })}
+        >
+          Decrement
+        </button>
+        <button
+          className={"btn btn-secondary m-3"}
+          onClick={() => dispatch({ type: "RESET" })}
+        >
+          Reset
+        </button>
+        </div>
+      </div>
 
-    </div>
-  )
+      <div className="mt-20px text-center ">
+        <input
+          type="number"
+          value={state.count}
+          onChange={(e) =>
+            dispatch({ type: "SETVALUE", payload: Number(e.target.value) || 0 })
+          }
+          
+        />
+      </div>
+    </>
+  );
 }
 
-export default UseReducer
+export default UseReducer;
