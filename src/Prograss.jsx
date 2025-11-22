2ad138b6733100a18a508aae4d612215ed699bfe
vigneshwaran <vigneshvenketasan@gmail.com>
2025-11-22 21:25:41 +0530

    prograss bar

--- src/Prograss.jsx
+++ src/Prograss.jsx
@@ -13,12 +13,32 @@ function Prograss() {
   }, [count]);
 
   return (
-    <div style={{textAlign:"center"}}>
+    <div style={{ position: "absolute", top: "30%", right: "30%" }}>
       <h2 style={{ textAlign: "center" }}>prograss bar</h2>
       <br />
-         
+      <div
+        style={{
+          width: "200px",
+          height: "40px",
+          border: "2px solid black",
+          textAlign: "center",
+          borderRadius: "30px ",
+          overflow: "hidden",
+        }}
+      >
+        <div
+          style={{
+            height: "100%",
+            backgroundColor: "blue",
+            width: `${count}% `,
+            textAlign: "center",
+            padding: "10px",
+          }}
+        >
+          {count}
+        </div>
+      </div>
     </div>
-   
   );
 }
 
